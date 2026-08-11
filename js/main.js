46c8af68195d3edebf612398edbb6b90aad60e4d
Nicole Njoroge <Nicole.Njoroge@ncbagroup.com>
2026-08-11 12:05:18 +0300

    resolving redirect issue

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var MSAL_CONFIG = {
   auth: {
     clientId: "e7b4c1f3-119f-4a5c-9a83-eca6314a7926",
     authority: "https://login.microsoftonline.com/6a28e8b9-ea23-417c-b7c9-7d38478b2a89",
-    redirectUri: window.location.origin,
+    redirectUri: 'https://gray-flower-070093b03.7.azurestaticapps.net/',
   },
   cache: {
     cacheLocation: "sessionStorage",
